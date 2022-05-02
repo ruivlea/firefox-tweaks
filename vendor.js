3aa26ac92108373a9f78cab1d72ed0124218ba25
ruivlea <ruivlea@gmail.com>
2022-05-02 17:39:48 +0700

    first commit

---
+++ vendor.js
@@ -0,0 +1,5 @@
+// Style
+pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+pref("widget.non-native-theme.gtk.scrollbar.round-thumb", false);
+pref("browser.uidensity", 1);
+pref("devtools.theme", "dark");
